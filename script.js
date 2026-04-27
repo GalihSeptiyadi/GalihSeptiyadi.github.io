18b219f97dec0c2edeeb415ecd1c58ad1c5e4778
GalihSeptiyadi <galih.septiyadi140904@gmail.com>
2026-04-27 19:54:37 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scroll saat klik menu
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        const targetId = this.getAttribute('href');
+        const target = document.querySelector(targetId);
+
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
